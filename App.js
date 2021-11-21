563820a02d37197a254aca56c667d4260496fd68
jtan334 <60947320+jtan334@users.noreply.github.com>
2021-11-20 21:11:18 -0500

    Finished User Profile

--- App.js
+++ App.js
@@ -34,6 +34,7 @@ function App() {
               elevation: 10,
             },
             headerTitleStyle: {
+              
               color: '#161b33',
               fontSize: 35,
               fontWeight: 'bold',
@@ -95,7 +96,21 @@ function App() {
           }} />
         <stack.Screen
           name="UserProfile"
-          component={user} />
+          component={user} 
+          options={{
+            headerTitle: 'User Profile',
+            headerStyle: {
+              backgroundColor: '#a7cdbd',
+              height: 100,
+              borderRadius: 2,
+              elevation: 10,
+            },
+            headerTitleStyle: {
+              color: '#161b33',
+              fontSize: 30,
+            },
+          }}/>
+          
         <stack.Screen
           name="Register"
           component={register} />
